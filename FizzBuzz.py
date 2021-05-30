a82e1a0f500fdd6f429548d3234258535b2b8f0d
Linshengyi-William <sunlins@oregonstate.edu>
2021-05-29 22:31:54 -0700

    Finished implementation for test 1.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,13 @@
 def FizzBuzz():
-    pass
+    lis = [None]*100
+    for i in range(len(lis)):
+        if i % 3 == 0:
+            #print("Fizz")
+            lis[i] = "Fizz"
+            #print("At index",i,":",lis[i],"\n")
+    return lis
+
+
+#FizzBuzz()
+
+
--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,7 +4,7 @@ import FizzBuzz
 class TestCase(unittest.TestCase):
     def test_Fizz(self):
         result = FizzBuzz.FizzBuzz()
-        for i in len(result):
+        for i in range(len(result)):
             if i % 3 == 0:
                 self.assertEqual("Fizz",result[i])
 

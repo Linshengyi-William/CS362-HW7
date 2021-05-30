fbe58a4e64d6843e326b3c9932342679e0156a3b
Linshengyi-William <sunlins@oregonstate.edu>
2021-05-29 23:53:24 -0700

    Wrote 2nd test. Check if result is right or not

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,15 +4,22 @@ import FizzBuzz
 class TestCase(unittest.TestCase):
     def test_Fizz(self):
         result = FizzBuzz.FizzBuzz()
-        for i in range(len(result)):
+        for i in range(1,101):
             if i % 3 == 0:
                 self.assertEqual("Fizz",result[i])
 
-    def test_Buzz(self):
-        result = FizzBuzz()
-        for i in range(len(result)):
-            if i % 5 == 0:
-                self.assertEqual("Buzz", result[i])
+    def test_FizzBuzz(self):
+        result = FizzBuzz.FizzBuzz()
+        for  i in range(1,101):
+            if i % 3 == 0 and i % 5 == 0:
+                self.assertEqual("FizzBuzz",result[i])    
+            elif i % 3 == 0:
+                self.assertEqual("Fizz",result[i])
+            elif i % 5 == 0:
+                self.assertEqual("Buzz",result[i])  
+            else:
+                self.assertEqual(i,result[i])
+                
 
 
 
